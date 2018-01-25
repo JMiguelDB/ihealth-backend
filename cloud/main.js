84efff25986e6aeadb431cdb58bce990c9f6fff8
Jose Miguel <jose.miguel.db.17894@gmail.com>
2018-01-25 11:14:59 +0100

    Añadida conexcion Firebase para notificaciones push en Android

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,21 @@
 Parse.Cloud.define('hello', function(req, res) {
     res.success('Hi');
   });
+
+// With Legacy Backbone callbacks
+Parse.Push.send({
+  where: query,
+  data: {
+    alert: 'Test',
+    badge: 1,
+    sound: 'default'
+  }
+}, {
+  useMasterKey: true,
+  success: function() {
+    // Push sent!
+  },
+  error: function(error) {
+    // There was a problem :(
+  }
+});
